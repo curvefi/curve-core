c43495e4c1bef062ace93daebc438bb27f1e6f48
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2024-07-19 12:08:58 +0200

    up address_provider (since smol change was made) and add deployment script

---
+++ scripts/deploy/registries/address_provider.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from scripts.deploy.utils import deploy_contract
+from settings.config import BASE_DIR
+
+
+def deploy_router(chain_name: str):
+    return deploy_contract(Path(BASE_DIR, "contracts", "registries", "address_provider"), chain_name)
