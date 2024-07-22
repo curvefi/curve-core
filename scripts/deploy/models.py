77d594e3fc2d66ae2fa564eee0f00f2c035130a5
Oleg <40476427+amfet42@users.noreply.github.com>
2024-07-22 17:31:05 +0200

    fix address provider address

--- scripts/deploy/models.py
+++ scripts/deploy/models.py
@@ -8,4 +8,4 @@ class CurveNetworkSettings:
     dao_ownership_contract: Address
     fee_receiver_address: Address
     metaregistry_address: Address | None = None  # TODO: add from deployments
-    address_provider: Address = "0x0000000022d53366457f9d5e68ec105046fc4383"  # TODO: add from deployments
+    address_provider: Address = "0x0000000022D53366457F9d5E68Ec105046FC4383"  # TODO: add from deployments
