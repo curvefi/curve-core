71aab246f094234b7fb023b3661845013a2251c6
Oleg <40476427+amfet42@users.noreply.github.com>
2024-08-13 22:54:16 +0200

    add postdeploy tests

--- scripts/tests/post_deploy/__init__.py
+++ scripts/tests/post_deploy/__init__.py
@@ -1,15 +1,16 @@
 import logging
 from pathlib import Path
 
-import yaml
-
+from scripts.deploy.deployment_file import YamlDeploymentFile
 from settings.config import BASE_DIR, get_chain_settings
 
 from .amm.stableswap import test_stableswap_deployment
 from .amm.tricrypto import test_tricrypto_deployment
 from .amm.twocrypto import test_twocrypto_deployment
+from .gauge import test_gauge_deployment
 from .helpers import test_helpers_deployment
 from .registries import test_registries_deployment
+from .xgov import test_xgov_deployment
 
 logger = logging.getLogger(__name__)
 
@@ -19,24 +20,30 @@ def test_post_deploy(chain: str):
 
     logger.info("Starting post-deployment tests...")
 
-    with open(Path(BASE_DIR, "deployments", f"{chain}.yaml"), "r") as file:
-        deployments = yaml.safe_load(file)
-
     chain_settings = get_chain_settings(chain)
+    deployment_file_path = Path(BASE_DIR, "deployments", f"{chain_settings.network_name}.yaml")
+    deployment_file = YamlDeploymentFile(deployment_file_path)
+    deployment = deployment_file.get_deployment_config()
 
-    test_stableswap_deployment(deployments["contracts"]["amm"]["stableswap"])
+    test_stableswap_deployment(deployment)
     logger.info("Stableswap tests ... PASSED")
 
-    test_twocrypto_deployment(deployments["contracts"]["amm"]["twocryptoswap"])
+    test_twocrypto_deployment(deployment)
     logger.info("Twocrypto tests ... PASSED")
 
-    test_tricrypto_deployment(deployments["contracts"]["amm"]["tricryptoswap"])
+    test_tricrypto_deployment(deployment)
     logger.info("Tricrypto tests ... PASSED")
 
-    test_helpers_deployment(deployments["contracts"]["helpers"])
+    test_helpers_deployment(deployment)
     logger.info("Helpers tests ... PASSED")
 
-    test_registries_deployment(deployments["contracts"], chain_settings)
+    test_registries_deployment(deployment, chain_settings)
     logger.info("Registries tests ... PASSED")
+
+    test_xgov_deployment(deployment)
+    logger.info("Xgov tests ... PASSED")
+
+    test_gauge_deployment(deployment)
+    logger.info("Gauge tests ... PASSED")
 
     logger.info("Post-deployment tests are finished.")
--- scripts/tests/post_deploy/gauge.py
+++ scripts/tests/post_deploy/gauge.py
@@ -2,10 +2,14 @@ from scripts.deploy.deployment_file import DeploymentConfig
 from scripts.tests.post_deploy.utils import check_contracts, get_contract
 
 
-def test_helpers_deployment(deployment: DeploymentConfig):
-    current_deployment = deployment.contracts.helpers
+def test_gauge_deployment(deployment: DeploymentConfig):
+    current_deployment = deployment.contracts.gauge.child_gauge
     contracts = {
         k: {**v, "contract": get_contract(v["contract_github_url"], v["address"])}
         for k, v in current_deployment.model_dump().items()
     }
     check_contracts(contracts)
+
+    factory = contracts["factory"]["contract"]
+
+    assert factory.get_implementation() == contracts["implementation"]["address"]
