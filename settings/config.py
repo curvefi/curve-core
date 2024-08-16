8572b43f86446185394a48b869af08801dd3c624
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2024-08-16 10:38:23 +0200

    fix preset params

--- settings/config.py
+++ settings/config.py
@@ -54,14 +54,14 @@ class ChainConfig(BaseSettings):
 
 
 class CryptoPoolPresets(BaseModel):
-    A: int = 400000
-    gamma: int = 145 * 10**12
-    mid_fee: int = 26 * 10**8
-    out_fee: int = 45 * 10**8
-    fee_gamma: int = 230 * 10**12
-    allowed_extra_profit: int = 2 * 10**12
-    adjustment_step: int = 146 * 10**12
-    ma_exp_time: int = 600
+    A: int = 20000000
+    gamma: int = 1000000000000000
+    mid_fee: int = 5000000
+    out_fee: int = 45000000
+    fee_gamma: int = 5000000000000000
+    allowed_extra_profit: int = 10000000000
+    adjustment_step: int = 5500000000000
+    ma_exp_time: int = 866
     initial_price: int = 10**18
 
 
