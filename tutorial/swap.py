d0a5c75ac9a4a2c76b699932b8dea18648103dc1
Oleg <40476427+amfet42@users.noreply.github.com>
2024-08-24 17:59:41 +0200

    add swap and gauge examples

--- tutorial/swap.py
+++ tutorial/swap.py
@@ -12,10 +12,12 @@ boa.env.add_account(account)
 
 # Test pool deployed, change address of deployed contract here
 POOL_ADDRESS = "0x8AAC9F7068d2942E4Be0b979D98e098D9C42075D"
+VIEWS = "0xd05b37dFe4c0377dbCb8030eAD07b41888716fE4"
 
 pool = boa.load_partial(
     Path(BASE_DIR, "contracts", "amm", "twocryptoswap", "implementation", "implementation_v_210.vy")
 ).at(POOL_ADDRESS)
+views = boa.load_partial(Path(BASE_DIR, "contracts", "amm", "twocryptoswap", "views", "views_v_200.vy")).at(VIEWS)
 
 token1_address = pool.coins(0)
 token2_address = pool.coins(1)
@@ -23,12 +25,12 @@ token2_address = pool.coins(1)
 token1 = boa.load_partial(Path(BASE_DIR, "tutorial", "contracts", "ERC20mock.vy")).at(token1_address)
 token2 = boa.load_partial(Path(BASE_DIR, "tutorial", "contracts", "ERC20mock.vy")).at(token2_address)
 
-AMOUNT = 100_000 * 10**18
+AMOUNT = 1_000 * 10**18
 
 assert token1.balanceOf(account.address) >= AMOUNT, "Not enough tokens to add"
-assert token2.balanceOf(account.address) >= AMOUNT, "Not enough tokens to add"
+
+amount_out = views.get_dy(0, 1, AMOUNT, POOL_ADDRESS)
 
 token1.approve(POOL_ADDRESS, AMOUNT)
-token2.approve(POOL_ADDRESS, AMOUNT)
 
-pool.add_liquidity([AMOUNT, AMOUNT], 0)
+pool.exchange(0, 1, AMOUNT, amount_out)
