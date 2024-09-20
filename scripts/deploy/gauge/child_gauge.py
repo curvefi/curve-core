091d18ea6fe820209ce5c5a8ec45f2bc27dc3db0
Roman Agureev <Agureev.RS@phystech.edu>
2024-09-20 22:44:17 +0300

    feat: update gauge factory

--- scripts/deploy/gauge/child_gauge.py
+++ scripts/deploy/gauge/child_gauge.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from scripts.deploy.constants import ROOT_GAUGE_FACTORY, ROOT_GAUGE_IMPLEMENTATION, ZERO_ADDRESS
 from scripts.deploy.deployment_utils import deploy_contract
 from scripts.logging_config import get_logger
 from settings.config import BASE_DIR, ChainConfig
@@ -13,6 +14,9 @@ def deploy_liquidity_gauge_infra(chain_settings: ChainConfig):
     child_gauge_factory = deploy_contract(
         chain_settings,
         Path(BASE_DIR, "contracts", "gauge", "child_gauge", "factory"),
+        ROOT_GAUGE_FACTORY,
+        ROOT_GAUGE_IMPLEMENTATION,
+        chain_settings.dao.crv or ZERO_ADDRESS,  # CRV
     )
     child_gauge_implementation = deploy_contract(
         chain_settings,
@@ -28,9 +32,9 @@ def deploy_liquidity_gauge_infra(chain_settings: ChainConfig):
         logger.info(f"Set liquidity child gauge implementation to {child_gauge_implementation.address}.")
 
     if chain_settings.dao and chain_settings.dao.crv:
-        crv_address = child_gauge_factory._storage.CRV.get()
+        crv_address = child_gauge_factory._storage.crv.get()
         if crv_address != chain_settings.dao.crv:
-            child_gauge_factory.set_crv_address(chain_settings.dao.crv)
+            child_gauge_factory.set_crv(chain_settings.dao.crv)
 
     logger.info("Liquidity Gauge Factory infra deployed.")
 
