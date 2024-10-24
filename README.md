084e9bd5bc23756e2669351c47c23a85eb2c60e7
fromstupig <tnphong.tech@gmail.com>
2024-10-24 07:02:36 +0700

    add swap script

--- README.md
+++ README.md
@@ -104,7 +104,7 @@ verification by API.
 #### Adding pools
 Make sure you have `env` file at `/settings` directory.
 
-Deployed pool (cryptoswap) using command
+##### Deployed pool (cryptoswap) using command
 ```
 python manage.py deploy crypto_pool {chain} {pool name} {pool symbol} {coins separated by comma}
 ```
@@ -116,7 +116,7 @@ FXS/dUSD
 python manage.py deploy crypto_pool fraxtal_testnet dTrinity_FXS_dUSD FXSDUSD 0x98182ec55Be5091d653F9Df016fb1070add7a16E,0x4D6E79013212F10A026A1FB0b926C9Fd0432b96c
 ```
 
-Add liquidity using command
+##### Add liquidity using command
 ```
 python add_liquidity.py --pool_address {pool_address} --amount_token_0 {amount_with_decimals} --amount_token_1 {amount_with_decimals}
 ```
@@ -126,4 +126,16 @@ Example
 FXS/dUSD
 
 python add_liquidity.py --pool_address 0xD978195666B3863Bed21C240f260d0F8bBa3250b --amount_token_0 2000000000000000000 --amount_token_1 1500000
+```
+
+##### Swap using command
+```
+python swap.py --pool_address {pool_address} --views_address {views_address} --amount_token_0 {amount_with_decimals}
+```
+
+You can find views address in deployment file at [deployments](/deployments) directory.
+
+Example
+```
+python swap.py --pool_address 0x1BBB5CAf76868698F00056f48f77ba13cfc5fE8D --views_address 0x0D5824948C879Fa459e41008F66E1Efc9211Cc32 --amount_token_0 4421718
 ```
--- swap.py
+++ swap.py
@@ -1,46 +1,40 @@
 from pathlib import Path
 
 import boa
+import argparse
 from eth_account import Account
 
 from settings.config import BASE_DIR, settings
-import argparse
 
 boa.set_network_env(settings.WEB3_PROVIDER_URL)
 account = Account.from_key(settings.DEPLOYER_EOA_PRIVATE_KEY)
 boa.env.add_account(account)
 
-parser = argparse.ArgumentParser(description="Add liquidity to a pool")
+parser = argparse.ArgumentParser(description="Swap tokens in a pool")
 parser.add_argument("--pool_address", type=str, required=True, help="Address of the pool")
-parser.add_argument("--amount_token_0", type=float, required=True, help="Amount of token 0 to add")
-parser.add_argument("--amount_token_1", type=float, required=True, help="Amount of token 1 to add")
+parser.add_argument("--views_address", type=str, required=True, help="Address of the views contract")
+parser.add_argument("--amount_token_0", type=int, required=True, help="Amount of token 0 to swap")
 
 args = parser.parse_args()
 
 POOL_ADDRESS = args.pool_address
-AMOUNT_TOKEN_0 = int(args.amount_token_0)
-AMOUNT_TOKEN_1 = int(args.amount_token_1)
-
+VIEWS = args.views_address
+AMOUNT_TOKEN_0 = args.amount_token_0
 pool = boa.load_partial(
     Path(BASE_DIR, "contracts", "amm", "twocryptoswap", "implementation", "implementation_v_210.vy")
 ).at(POOL_ADDRESS)
+views = boa.load_partial(Path(BASE_DIR, "contracts", "amm", "twocryptoswap", "views", "views_v_200.vy")).at(VIEWS)
 
 token0_address = pool.coins(0)
 token1_address = pool.coins(1)
 
-print(f"Token 0: {token0_address}")
-print(f"Token 1: {token1_address}")
-
 token0 = boa.load_partial(Path(BASE_DIR, "tutorial", "contracts", "ERC20mock.vy")).at(token0_address)
 token1 = boa.load_partial(Path(BASE_DIR, "tutorial", "contracts", "ERC20mock.vy")).at(token1_address)
 
-print(f"Token 0 balance: {token0.balanceOf(account.address)}")
-print(f"Token 1 balance: {token1.balanceOf(account.address)}")
-
 assert token0.balanceOf(account.address) >= AMOUNT_TOKEN_0, "Not enough tokens to add"
-assert token1.balanceOf(account.address) >= AMOUNT_TOKEN_1, "Not enough tokens to add"
+
+amount_out = views.get_dy(0, 1, AMOUNT_TOKEN_0, POOL_ADDRESS)
 
 token0.approve(POOL_ADDRESS, AMOUNT_TOKEN_0)
-token1.approve(POOL_ADDRESS, AMOUNT_TOKEN_1)
 
-pool.add_liquidity([AMOUNT_TOKEN_0, AMOUNT_TOKEN_1], 0)
+pool.exchange(0, 1, AMOUNT_TOKEN_0, amount_out)
